f653b83d1c018f4fc569968bc2f3d4783e2cbeb5
JohnsonQerja <johnson@qerja.com>
2023-02-02 13:36:18 +0700

    commit fix homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { API } from '../endpoints/api';
 // import { PHOTOS, COMMENT } from '../endpoints/url';
 import Cookies from "js-cookie";
 import Header from "../components/header";
 import GalleryGrid from "../components/gallery-grid";
 import ThumbnailImg from "../components/thumbnail-img";
-import BottomSheet from "../components/bottomsheet";
-import PhotoPreview from "../components/photo-preview";
-import CommentsSection from "../components/comments-section";
 import { Icon } from '@iconify/react';
 import loadingIcon from '@iconify/icons-mdi/loading';
 import arrowDownOutline from '@iconify/icons-ion/arrow-down-outline';
@@ -19,10 +15,6 @@ export default function Home({ photosData }) {
   const [data, setData] = useState();
   const [totalPost, setTotalPost] = useState(0);
   const [fetchData, setFetchData] = useState(true);
-  const [photo, setPhoto] = useState();
-  const [comments, setComments] = useState();
-  const [fetchPhoto, setFetchPhoto] = useState(false);
-  const [showBottomSheet, setShowBottomSheet] = useState(false);
 
   useEffect(() => {
     const user = Cookies.get('user');
@@ -74,70 +66,6 @@ export default function Home({ photosData }) {
     }
   };
   
-  /*
-  const fetchPhotoDetail = async (photoId) => {
-    setPhoto();
-    setShowBottomSheet(true);
-    try {
-      setFetchPhoto(true);
-      const reqBody = {
-        query: `
-          query photo($photoId: ID!){
-            photo(photoId: $photoId) {
-              _id
-              imageUrl
-              caption
-              user {
-                _id
-                name
-              }
-              likes {
-                _id
-                user {
-                  _id
-                }
-              }
-              comments {
-                _id
-                message
-                user {
-                  _id
-                  name
-                }
-                reply {
-                  _id
-                  message
-                  user {
-                    _id
-                    name
-                  }
-                }
-                likes {
-                  _id
-                }
-              }
-            }
-          }
-        `,
-        variables: {
-          photoId,
-        }
-      };
-      const photo = await API.post(process.env.API_URL, reqBody);
-      setPhoto(photo.data.photo);
-      setComments(photo.data.photo.comments);
-      // const photo = await API.get(PHOTOS + `/${photoId}`);
-      // const comments = await API.get(COMMENT + `/${photoId}`);
-      // setPhoto(photo.data.data);
-      // setComments(comments.data.data);
-      setFetchPhoto(false);
-    } catch (error) {
-      setFetchPhoto(false);
-      console.log(error);
-    }
-  }
-  */
-
   const windowScroll = () => {
     let docOffsetheight = document.body.offsetHeight;
     if ((window.innerHeight + window.scrollY) >= (docOffsetheight - 124)) {
@@ -178,24 +106,6 @@ export default function Home({ photosData }) {
           ) : null
         }
       </div>
-      <BottomSheet
-        open={showBottomSheet}
-        height="80"
-        onDismiss={() => setShowBottomSheet(false)}
-      >
-        {
-          fetchPhoto ? (
-            <div className="flex min-w-full min-h-full justify-center items-center">
-              <Icon icon={loadingIcon} className="inline-block animate-spin text-lg align-text-top"/> fetching...
-            </div>
-          ) : photo ? (
-            <>
-              <PhotoPreview photo={photo} canEdit={user && photo.user._id === user._id ? true : false}/>
-              <CommentsSection photoId={photo._id} comments={comments} user={user} className="mt-4"/>
-            </>
-          ) : null
-        }
-      </BottomSheet>
     </div>
   )
 }
@@ -235,13 +145,11 @@ export async function getServerSideProps (context) {
       }
     }
   } catch (error) {
-    console.log(error);
     return {
-      props: {}
-      // redirect: {
-      //     destination: '/505',
-      //     statusCode: 307
-      // }
+      redirect: {
+          destination: '/505',
+          statusCode: 307
+      }
     }
   }
 }
