3c96d3a01422121093a7377175a0c43159cf782c
johnson286 <johnson.lin.286@gmail.com>
2023-04-28 16:16:32 +0700

    commit fix view other user profile bug

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Cookies from "js-cookie";
-import { API } from '../../endpoints/api';
+import { API } from "../../endpoints/api";
 // import { PHOTOS, COMMENT } from "../../endpoints/url";
 import Header from "../../components/header";
 import PhotoPreview from "../../components/photo-preview";
-import { Icon } from '@iconify/react';
-import loadingIcon from '@iconify/icons-mdi/loading';
-import arrowDownOutline from '@iconify/icons-ion/arrow-down-outline';
+import { Icon } from "@iconify/react";
+import loadingIcon from "@iconify/icons-mdi/loading";
+import arrowDownOutline from "@iconify/icons-ion/arrow-down-outline";
 import Avatar from "../../components/avatar";
 import Modal from "../../components/modal";
 import Button from "../../components/button";
-import BottomSheet from '../../components/bottomsheet';
+import BottomSheet from "../../components/bottomsheet";
 import CommentsSection from "../../components/comments-section";
 
 const Profile = ({ photosData }) => {
@@ -30,10 +30,11 @@ const Profile = ({ photosData }) => {
   const [fetchingComments, setFetchingComments] = useState(false);
 
   useEffect(() => {
-    const user = Cookies.get('user');
+    const user = Cookies.get("user");
     if (user) {
-      setUser(JSON.parse(user))
-    } fetchUserData();
+      setUser(JSON.parse(user));
+    }
+    fetchUserData();
   }, []);
 
   useEffect(() => {
@@ -56,13 +57,13 @@ const Profile = ({ photosData }) => {
         }
       `,
       variables: {
-        userId: router.query.id
-      }
+        userId: router.query.id,
+      },
     };
     const response = await API.post(process.env.API_URL, reqBody);
     const result = response.data.profile;
-    setUser({id: result._id, name: result.name, email: result.email});
-  }
+    setUser({ id: result._id, name: result.name, email: result.email });
+  };
 
   const fetchUserPhotos = async () => {
     try {
@@ -88,18 +89,15 @@ const Profile = ({ photosData }) => {
         `,
         variables: {
           userId: user.id,
-          skip: data && data.length || 0,
-          limit: 2
-        }
+          skip: (data && data.length) || 0,
+          limit: 2,
+        },
       };
       const response = await API.post(process.env.API_URL, reqBody);
       const result = response.data.userPhotos;
       if (result.data && result.data.length > 0) {
-        setData(prev => [
-          ...prev,
-          ...result.data,
-        ]);
-      };
+        setData((prev) => [...prev, ...result.data]);
+      }
       setFetchData(false);
     } catch (error) {
       setFetchData(false);
@@ -110,17 +108,17 @@ const Profile = ({ photosData }) => {
   const showConfirmWindow = (photoId) => {
     setPickedPhotoId(photoId);
     setShowConfirm(true);
-  }
+  };
 
   const hideConfirmWindow = () => {
     setShowConfirm(false);
-  }
+  };
 
   const deleteUserPhoto = async () => {
     try {
       setOnRemove(true);
       const allPhoto = data;
-      const photo = data.find(item => item._id === pickedPhotoId);
+      const photo = data.find((item) => item._id === pickedPhotoId);
       const indexPhoto = allPhoto.indexOf(photo);
       const reqBody = {
         query: `
@@ -134,7 +132,7 @@ const Profile = ({ photosData }) => {
         `,
         variables: {
           photoId: pickedPhotoId,
-        }
+        },
       };
       const response = await API.post(process.env.API_URL, reqBody);
       console.log(response);
@@ -147,7 +145,7 @@ const Profile = ({ photosData }) => {
       setOnRemove(false);
       hideConfirmWindow();
     }
-  }
+  };
 
   const dismissBottomSheet = () => {
     setPickedPhotoId();
@@ -159,7 +157,7 @@ const Profile = ({ photosData }) => {
   const fetchComments = async (photoId) => {
     setShowComments(true);
     setPickedPhotoId(photoId);
-    const photoData = data.find(photo => photo._id === photoId);
+    const photoData = data.find((photo) => photo._id === photoId);
     setPickedPhotoData(photoData);
     try {
       setFetchingComments(true);
@@ -202,8 +200,8 @@ const Profile = ({ photosData }) => {
         variables: {
           photoId: photoId,
           skip: 0,
-          limit: 4
-        }
+          limit: 4,
+        },
       };
       const response = await API.post(process.env.API_URL, reqBody);
       const result = response.data.comments;
@@ -252,116 +250,131 @@ const Profile = ({ photosData }) => {
           variables: {
             photoId: pickedPhotoId,
             skip: commentsData.length,
-            limit: 4
-          }
-        }; 
+            limit: 4,
+          },
+        };
         const response = await API.post(process.env.API_URL, reqBody);
         const result = response.data.comments;
-        setCommentsData(prev => [
-          ...prev,
-          ...result.data
-        ]);
+        setCommentsData((prev) => [...prev, ...result.data]);
       } catch (error) {
-        throw error
+        throw error;
       }
     }
-  }
+  };
 
   const windowScroll = () => {
     let docOffsetheight = document.body.offsetHeight;
-    if ((window.innerHeight + window.scrollY) >= docOffsetheight) {
+    if (window.innerHeight + window.scrollY >= docOffsetheight) {
       if (data && data.length < totalPost && !fetchData) fetchUserPhotos();
     }
   };
 
   useEffect(() => {
-    document.addEventListener('scroll', windowScroll);
-    return () => document.removeEventListener('scroll', windowScroll);
+    document.addEventListener("scroll", windowScroll);
+    return () => document.removeEventListener("scroll", windowScroll);
   }, [windowScroll]);
-  
+
   return (
     <div className="dashboard">
-      <Header backBtn backRoute={'/'} fixed title={user && user.name}/>
-      <div className={`flex flex-col min-h-screen pb-20${data && !data.length > 0 ? ' justify-between' : ''}`}>
+      <Header backBtn backRoute={"/"} fixed title={user && user.name} />
+      <div
+        className={`flex flex-col min-h-screen pb-20${
+          data && !data.length > 0 ? " justify-between" : ""
+        }`}
+      >
         <div className="flex px-5 md:px-0 pt-16 pb-4">
-          <Avatar shape="circle" size="lg" border alt={user && user.name}/>
+          <Avatar shape="circle" size="lg" border alt={user && user.name} />
           <div className="flex flex-col flex-1 justify-center items-center text-2xl">
             {totalPost}
-            <p>{`Post${data && data.length > 1 ? 's' : ''}`}</p>
+            <p>{`Post${data && data.length > 1 ? "s" : ""}`}</p>
           </div>
-          <div className="flex-1"/>
-          <div className="flex-1"/>
+          <div className="flex-1" />
+          <div className="flex-1" />
         </div>
-        {
-          data && data.length > 0 ? (
-            <div className="px-5 md:px-0">
-              {
-                data.map((item, i) => (
-                  <div className="border-b pb-4 mb-4" key={i}>
-                    <PhotoPreview
-                      photo={item}
-                      canEdit
-                      canDelete
-                      deleteCallback={showConfirmWindow}
-                      showComments
-                      commentsCallback={fetchComments}
-                    />
-                  </div>
-                ))
-              }
-            </div>
-          ) : !fetchData ? (
-            <div className="flex flex-col justify-end items-center">
-              <p>No post found! Start post your image here...</p>
-              <Icon icon={arrowDownOutline} width="38" height="38"/>
-            </div>
-          ) : null
-        }
-        {
-          fetchData ? (
-            <div className="flex justify-center items-center py-5">
-              <Icon icon={loadingIcon} className="inline-block animate-spin text-lg align-text-top"/> fetching...
-            </div>
-          ) : null
-        }
+        {data && data.length > 0 ? (
+          <div className="px-5 md:px-0">
+            {data.map((item, i) => (
+              <div className="border-b pb-4 mb-4" key={i}>
+                <PhotoPreview
+                  photo={item}
+                  canEdit
+                  canDelete
+                  deleteCallback={showConfirmWindow}
+                  showComments
+                  commentsCallback={fetchComments}
+                />
+              </div>
+            ))}
+          </div>
+        ) : !fetchData ? (
+          <div className="flex flex-col justify-end items-center">
+            <p>No post found! Start post your image here...</p>
+            <Icon icon={arrowDownOutline} width="38" height="38" />
+          </div>
+        ) : null}
+        {fetchData ? (
+          <div className="flex justify-center items-center py-5">
+            <Icon
+              icon={loadingIcon}
+              className="inline-block animate-spin text-lg align-text-top"
+            />{" "}
+            fetching...
+          </div>
+        ) : null}
       </div>
       <BottomSheet open={showComments} onDismiss={dismissBottomSheet}>
-        {
-          user && pickedPhotoData && commentsData && (
-            <>
-              <h3 className="text-xl font-medium mb-4">Comments</h3>
-              <div className="flex items-center mb-4">
-                <Avatar size="sm" shape="circle" border alt={user.name}/>
-                <p className="flex flex-col px-3">
-                  <strong className="inline-block mr-2">{user.name}</strong>
-                  {pickedPhotoData.caption}
-                </p>
-              </div>
-              <CommentsSection
-                photoId={pickedPhotoId}
-                comments={commentsData}
-                total={totalComments}
-                // user={user}
-                loadMoreCallback={fetchMoreComments}
-              />
-            </>
-          )
-        }
-        {
-          fetchingComments ? (
-            <div className="flex min-w-full min-h-full justify-center items-center">
-              <Icon icon={loadingIcon} className="inline-block animate-spin text-lg align-text-top"/> fetching...
+        {user && pickedPhotoData && commentsData && (
+          <>
+            <h3 className="text-xl font-medium mb-4">Comments</h3>
+            <div className="flex items-center mb-4">
+              <Avatar size="sm" shape="circle" border alt={user.name} />
+              <p className="flex flex-col px-3">
+                <strong className="inline-block mr-2">{user.name}</strong>
+                {pickedPhotoData.caption}
+              </p>
             </div>
-          ) : null
-        }
+            <CommentsSection
+              photoId={pickedPhotoId}
+              comments={commentsData}
+              total={totalComments}
+              // user={user}
+              loadMoreCallback={fetchMoreComments}
+            />
+          </>
+        )}
+        {fetchingComments ? (
+          <div className="flex min-w-full min-h-full justify-center items-center">
+            <Icon
+              icon={loadingIcon}
+              className="inline-block animate-spin text-lg align-text-top"
+            />{" "}
+            fetching...
+          </div>
+        ) : null}
       </BottomSheet>
       <Modal show={showConfirm} size="sm" onDismiss={hideConfirmWindow}>
         <p className="mb-2">Are you sure want to delete this photo?</p>
         <div className="inline-block w-1/2 pr-1">
-          <Button size="sm" outline block disabled={onRemove} loading={onRemove} onClick={deleteUserPhoto}>Yes</Button>
+          <Button
+            size="sm"
+            outline
+            block
+            disabled={onRemove}
+            loading={onRemove}
+            onClick={deleteUserPhoto}
+          >
+            Yes
+          </Button>
         </div>
         <div className="inline-block w-1/2 pl-1">
-          <Button size="sm" block disabled={onRemove} onClick={hideConfirmWindow}>No</Button>
+          <Button
+            size="sm"
+            block
+            disabled={onRemove}
+            onClick={hideConfirmWindow}
+          >
+            No
+          </Button>
         </div>
       </Modal>
     </div>
@@ -370,9 +383,8 @@ const Profile = ({ photosData }) => {
 
 export default Profile;
 
-export async function getServerSideProps (context) {
+export async function getServerSideProps(context) {
   try {
-    const user = context.req.cookies.user ? JSON.parse(context.req.cookies.user) : null;
     const reqBody = {
       query: `
         query userPhotos($userId: ID!, $skip: Float, $limit: Float) {
@@ -393,23 +405,23 @@ export async function getServerSideProps (context) {
         }
       `,
       variables: {
-        userId: user ? user.id : context.query.id,
+        userId: context.params.id,
         skip: 0,
-        limit: 2
-      }
-    }
+        limit: 2,
+      },
+    };
     const response = await API.post(process.env.API_URL, reqBody);
     return {
       props: {
-        photosData: response.data
-      }
-    }
+        photosData: response.data,
+      },
+    };
   } catch (error) {
     return {
       redirect: {
-        destination: '/',
-        statusCode: 307
-      }
-    }
+        destination: "/",
+        statusCode: 307,
+      },
+    };
   }
 }
